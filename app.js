0fc03a105e1ed7f7e81af67521b72111f29f1d42
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-08 10:25:56 +0200

    filtry

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var json
+var tab
 async function getData(){
     const data = await fetch('https://restcountries.com/v3.1/all')
     json = await data.json()
@@ -7,65 +8,37 @@ async function getData(){
 var inuptValue 
 
 getData()
+
 function createList(){
     document.getElementById("main").innerHTML = ""
-    for(var i=0;i<json.length;i++){
-        if(json[i].name.common.toLowerCase().includes(inuptValue)){
-            const div = document.createElement("div")
-            div.classList.add("div")
-            const flag = document.createElement("img")
-            flag.classList.add("flag")
-            flag.src = json[i].flags.png
+    for(let i=0;i<json.length;i++){
+        for(let k=0;k<=filtr.length;k++){
+            if(json[i].region==filtr[k]){
 
-            const name = document.createElement("h2")
-            name.classList.add("name")
-            name.innerHTML = json[i].name.common
-
-            const capital = document.createElement("h3")
-            capital.classList.add("capital")
-            if(json[i].capital == undefined){
-                capital.innerHTML = "No capital"
-                capital.classList.add("red")
-            }
+                const div = document.createElement("div")
+                div.classList.add("div")
+                const flag = document.createElement("img")
+                flag.classList.add("flag")
+                flag.src = json[i].flags.png
+                
+                const name = document.createElement("h2")
+                name.classList.add("name")
+                name.innerHTML = json[i].name.common
+                
+                const capital = document.createElement("h3")
+                capital.classList.add("capital")
+                if(json[i].capital == undefined){
+                    capital.innerHTML = "No capital"
+                    capital.classList.add("red")
+                }
             else{
                 capital.innerHTML = json[i].capital
             }
-
+            
             const population = document.createElement("h3")
             population.classList.add("population")   
             population.innerHTML = json[i].population     
-
-            div.appendChild(name)
-            div.appendChild(flag)
-            div.appendChild(capital)
-            div.appendChild(population)
-            document.getElementById("main").appendChild(div)
-        }
-        else if(inuptValue = ""){
-            const div = document.createElement("div")
-            div.classList.add("div")
-            const flag = document.createElement("img")
-            flag.classList.add("flag")
-            flag.src = json[i].flags.png
-
-            const name = document.createElement("h2")
-            name.classList.add("name")
-            name.innerHTML = json[i].name.common
-
-            const capital = document.createElement("h3")
-            capital.classList.add("capital")
-            if(json[i].capital == undefined){
-                capital.innerHTML = "No capital"
-                capital.classList.add("red")
-            }
-            else{
-                capital.innerHTML = json[i].capital
-            }
-
-            const population = document.createElement("h3")
-            population.classList.add("population")   
-            population.innerHTML = json[i].population     
-
+            
             div.appendChild(name)
             div.appendChild(flag)
             div.appendChild(capital)
@@ -73,18 +46,24 @@ function createList(){
             document.getElementById("main").appendChild(div)
         }
     }
+    }
 }
 
-
-function find(){
-    console.log("click*")
-    inuptValue = document.getElementById("txtSearch").value.toLowerCase()
-    console.log(inuptValue)
+var filtr = []
+function filter(){
+    tab =[
+        {name:"Oceania",check:document.getElementById("oc").checked},
+        {name:"Africa",check:document.getElementById("af").checked},
+        {name:"Americas",check:document.getElementById("am").checked},
+        {name:"Antarctica",check:document.getElementById("an").checked},
+        {name:"Europe",check:document.getElementById("eu").checked},
+        {name:"Asia",check:document.getElementById("as").checked},
+    ]
+    filtr =[]
+    for(let j=0;j<tab.length;j++){
+        if(tab[j].check==true){
+            filtr.push(tab[j].name)            
+        }
+    }
     createList()
 }
-
-document.querySelector('#txtSearch').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      find()
-    }
-});
